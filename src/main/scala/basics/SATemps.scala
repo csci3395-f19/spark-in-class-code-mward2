cd2e7f685be3822f6629399ab9e740de3b476445
mward2 <mward2@trinity.edu>
2019-09-10 11:27:21 -0500

    Added SparkRDD files from in class

---
+++ src/main/scala/basics/SATemps.scala
@@ -0,0 +1,58 @@
+package basics
+import swiftvis2.plotting._
+import swiftvis2.plotting.styles.ScatterStyle
+import swiftvis2.plotting.renderer.SwingRenderer
+import swiftvis2.plotting.ColorGradient
+
+case class TempRow(day: Int, doy: Int, month: Int, year: Int, precip: Double, tave: Double, tmax: Double, tmin: Double)
+
+object SATemps{
+    def parseLine(line: String): TempRow ={
+        val p = line.split(",");
+        TempRow(p(0).toInt, p(1).toInt, p(2).toInt, p(4).toInt, p(5).toDouble, p(6).toDouble, p(7).toDouble, p(8).toDouble)
+    }
+
+    def main(args: Array[String]): Unit = {
+        val source = scala.io.Source.fromFile("/users/mlewis/CSCI3395-F19/InClassBD/data/SanAntonioTemps.csv")
+        val lines = source.getLines()
+        val data = lines.drop(2).map(parseLine).toArray
+        //data.take(5).foreach(println)
+
+        val hotDay1 = data.maxBy(_.tmax)
+        val hotDay2 = data.reduce((d1, d2) => if(d1.tmax > d2.tmax) d1 else d2)
+
+        val mostPrecip = data.maxBy(_.precip)
+
+        val rainyDays = data.filter(p => p.precip >= 1.0)
+        val (rainySum, rainyCount) = data.foldLeft((0.0, 0)){
+            case((sum, cnt), day) => 
+            if (day.precip >= 1) (sum + day.precip, cnt + 1) else (sum, cnt)
+        }
+
+        val fracRainyDays = (rainyDays.length.toDouble)/data.length.toDouble
+
+        val rainyTemps = rainyDays.foldLeft(0.0)(_ + _.tmax) / rainyDays.length
+
+        val months = data.groupBy(_.month)
+        val avgHotDays = months.mapValues((rows) => rows.map(_.tmax).sum/ rows.length)
+
+        val medianPrecipByMonth = months.mapValues(m => m.sortBy(_.precip).apply(m.length/2))
+        //medianPrecipByMonth.toSeq.sortBy(_._1).foreach(println)
+
+        val cg = ColorGradient(1946.0 -> RedARGB, 1975.0 -> BlueARGB, 2014.0 -> GreenARGB)
+        val sizes = data.map(_.precip * 2 + 2)
+
+        val tempByDayPlot = Plot.simple(
+            ScatterStyle(data.map(_.doy), data.map(_.tave), symbolWidth = sizes, symbolHeight = sizes, colors = cg(data.map(_.year))),
+            "SA Temps", "Day of Year", "Temps")
+        SwingRenderer(tempByDayPlot, 800, 800, true)
+
+        //println(hotDay1)
+        //println(hotDay2)
+        //println(mostPrecip)
+        //println(fracRainyDays)
+        //println(rainyTemps)
+        //println(avgHotDays.toSeq.sortBy(_._1).foreach(println)
+        //println(medianPrecipByMonth)
+    }
+}
